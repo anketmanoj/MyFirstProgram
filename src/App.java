004a45514f640367200d3455e67790b7228abba9
anketmanoj <anket.m@live.com>
2022-03-03 12:31:25 +0400

    working with math class in java

--- src/App.java
+++ src/App.java
@@ -1,70 +1,85 @@
 // public class App {
-//     public static void main(String[] args) throws Exception {
+// public static void main(String[] args) throws Exception {
 
-//         String x = "water";
-//         String y = "Koolaid";
-//         String temp;
+// String x = "water";
+// String y = "Koolaid";
+// String temp;
 
-//         temp = x;
-//         x = y;
-//         y = temp;
+// temp = x;
+// x = y;
+// y = temp;
 
-//         System.out.println("X is " + x);
-//         System.out.println("Y is " + y);
+// System.out.println("X is " + x);
+// System.out.println("Y is " + y);
 
-//     }
+// }
 // }
 
 // import java.util.Scanner;
 
 // // create a public class app that has a main method
 // public class App {
-//     // create a main method
-//     public static void main(String[] args) throws Exception {
+// // create a main method
+// public static void main(String[] args) throws Exception {
 
-//         Scanner scanner = new Scanner(System.in);
+// Scanner scanner = new Scanner(System.in);
 
-//         System.out.println("What is your name?");
-//         String name = scanner.nextLine();
+// System.out.println("What is your name?");
+// String name = scanner.nextLine();
 
-//         System.out.println("What is your age?");
-//         int age = scanner.nextInt();
-//         scanner.nextLine();
+// System.out.println("What is your age?");
+// int age = scanner.nextInt();
+// scanner.nextLine();
 
-//         System.out.println("What is your favorite color?");
-//         String color = scanner.next();
+// System.out.println("What is your favorite color?");
+// String color = scanner.next();
 
-//         System.out.println(
-//                 "Hello, " + name + "!" + " You are " + age + " years old." + " Your favorite color is " + color + ".");
+// System.out.println(
+// "Hello, " + name + "!" + " You are " + age + " years old." + " Your favorite
+// color is " + color + ".");
 
-//     }
+// }
 // }
 
-//* Expressions in Java
+// * Expressions in Java
 
 // public class App {
-//     public static void main(String[] args) throws Exception {
+// public static void main(String[] args) throws Exception {
 
-//         double friends = 10;
-//         friends = friends / 3;
+// double friends = 10;
+// friends = friends / 3;
 
-//         System.out.println(friends);
-//     }
+// System.out.println(friends);
+// }
 // }
 
 // * basic GUI app
-import javax.swing.JOptionPane;
+// import javax.swing.JOptionPane;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        String name = JOptionPane.showInputDialog("What is your name?");
-        JOptionPane.showMessageDialog(null, "Hello, " + name + "!");
+// public class App {
+// public static void main(String[] args) throws Exception {
+// String name = JOptionPane.showInputDialog("What is your name?");
+// JOptionPane.showMessageDialog(null, "Hello, " + name + "!");
 
-        int age = Integer.parseInt(JOptionPane.showInputDialog("How old are you?"));
-        JOptionPane.showMessageDialog(null, "You are " + age + " years old.");
+// int age = Integer.parseInt(JOptionPane.showInputDialog("How old are you?"));
+// JOptionPane.showMessageDialog(null, "You are " + age + " years old.");
 
-        double height = Double.parseDouble(JOptionPane.showInputDialog("How tall are you?"));
-        JOptionPane.showMessageDialog(null, "You are " + height + " inches tall.");
+// double height = Double.parseDouble(JOptionPane.showInputDialog("How tall are
+// you?"));
+// JOptionPane.showMessageDialog(null, "You are " + height + " inches tall.");
 
-    }
-}
+// }
+// }
+
+// * working with math class
+
+// public class App {
+// public static void main(String[] args) throws Exception {
+// double x = 3.14;
+// double y = -10;
+
+// double z = Math.PI;
+
+// System.out.println("The largest number is " + z);
+// }
+// }
